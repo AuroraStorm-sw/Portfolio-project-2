1afa5fc3735467149235d5092ce767b7d1de5c96
Aurora <aurorastorm22@proton.me>
2023-03-22 08:46:06 +0000

    testing button reaction with javascript, bug resolving accidentally clicking buttons with 0 opacity

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+let warriorScore = 0;
+let computerScore = 0;
+
+const startBtn = document.getElementById("start");
+startBtn.addEventListener('click', function() {
+    console.log('Start button is clicked');
+})
+
+function startGame() {
+
+}
+
