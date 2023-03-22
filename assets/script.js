12a1a56fa674778a4661988aa7e70df2784a24ec
Aurora <aurorastorm22@proton.me>
2023-03-22 14:23:51 +0000

    Add else if statement to pick out the users choice

--- assets/script.js
+++ assets/script.js
@@ -1,117 +1,71 @@
-    let warriorScore = 0;
-    let computerScore = 0;
+let warriorScore = 0;
+let computerScore = 0;
 
-    const startBtn = document.getElementById("start");
+let userHand;
 
-    const introScreen = document.querySelector(".intro-screen");
-    const matchScreen = document.querySelector(".playfield");
+const startBtn = document.getElementById("start");
 
-    const options = document.querySelectorAll(".options button");
-    const warriorHand = document.querySelector(".warrior-hand");
-    const computerHand = document.querySelector(".computer-hand");
- 
-    const computerOptions = ['shield', 'bow', 'sword'];
-  
-    // variables for updateScore function
-    const playerScores = document.querySelector('.warrior-score p');
-    const computerScores = document.querySelector('.computer-score p');
-  
-    // variables for comparing the score between user and computer
-    const winner = document.querySelector('.winner');
+const introScreen = document.querySelector(".intro-screen");
+const matchScreen = document.querySelector(".playfield");
 
-    startBtn.addEventListener('click', function() {
-        startGame();
-        console.log('Start button is clicked');
+const options = document.querySelectorAll(".options button");
+const warriorHand = document.querySelector(".warrior-hand");
+const computerHand = document.querySelector(".computer-hand");
+
+const playerScores = document.querySelector('.warrior-score p');
+const computerScores = document.querySelector('.computer-score p');
+
+const winner = document.querySelector('.winner');
+
+startBtn.addEventListener('click', function() {
+  startGame();
+  console.log('Start button is clicked');
+});
+
+//**Function that adds the hide class to the intro screen and removes the hide class
+//* from the match screen
+
+function startGame() {
+
+  console.log('game has begun');
+
+  introScreen.classList.add('hide');
+  matchScreen.classList.remove('hide');
+
+  playMatch();
+};
+
+function playMatch() {
+
+  const computerOptions = ['shield', 'bow', 'sword'];
+
+  options.forEach(option => {
+    option.addEventListener('click', function() {
+      const computerNumber = Math.floor(Math.random() * 3);
+      const computerChoice = computerOptions[computerNumber];
+
+      console.log(computerChoice);
+
+      /**Using 'else if' as a way to decide the players choice was inspired from 
+      * https://github.com/kpsdev1 / https://github.com/kpsdev1/Rock-Paper-Scissors/blob/main/assets/js/script.js
+      * I found this while searching for a tutorial/ideas for how to solve picking out what the user chose
+      */
+
+      if (this.getAttribute('data-type') === 'shield') userHand = 'shield';
+      else if (this.getAttribute('data-type') === 'bow') userHand = 'bow';
+      else userHand = 'sword';
+
+      console.log(userHand);
     });
+  });
+}
 
-    //**Function that adds the hide class to the intro screen and removes the hide class
-    //* from the match screen
-
-    function startGame() {
-
-        console.log('game has begun');
-
-        introScreen.classList.add('hide');
-        matchScreen.classList.remove('hide');
-
-        playMatch();
-    };
-
-    function playMatch() {
+function decideWinner() {
   
-        // APPLYING COMPUTER OPTIONS
+  if () {
     
-        options.forEach(option => {
-          option.addEventListener('click', function() {
-            const computerNumber = Math.floor(Math.random() * 3);
-            const computerChoice = computerOptions[computerNumber];
+  } else {
     
-            //Here is where we initialize compare hands
-    
-            compareHands(this.textContent, computerChoice);
-    
-            //Update images
-            warriorHand.src = `assets/img/${this.textContent}.jpg`;
-            computerHand.src = `assets/img/${computerChoice}.jpg`;
-          });
-        });
-    
-    }
-
-    function compareHands(playerChoice, computerChoice) {
-        //Update 'Choose your option' text
-  
-        //Checking for a tie
-        if (playerChoice === computerChoice) {
-          winner.textContent = 'It is a tie!';
-          return;
-        }
-  
-        // Check for rock
-        if (playerChoice === 'rock') {
-          if (computerChoice === 'scissors') {
-            winner.textContent = 'Player scores!';
-            pScore++;
-            updateScore();
-            return;
-          } else {
-            winner.textContent = 'Computer scores!';
-            cScore++;
-            updateScore();
-            return;
-          }
-        }
-  
-        //Check for paper
-        if (playerChoice === 'paper') {
-          if (computerChoice === 'scissors') {
-            winner.textContent = 'Computer scores!';
-            cScore++;
-            updateScore();
-            return;
-          } else {
-            winner.textContent = 'Player scores!';
-            pScore++;
-            updateScore();
-            return;
-          }
-        }
-  
-        //Check for scissors
-        if (playerChoice === 'scissors') {
-          if (computerChoice === 'rock') {
-            winner.textContent = 'Computer scores!';
-            cScore++;
-            updateScore();
-            return;
-          } else {
-            winner.textContent = 'Player scores!';
-            pScore++;
-            updateScore();
-            return;
-          }
-        }
-  
-      }
-
-
+  }
+  return result;
+}
