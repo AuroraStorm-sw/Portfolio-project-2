af5293bdd612145210942fd5216833005b805429
Aurora <aurorastorm22@proton.me>
2023-03-22 09:39:24 +0000

    Add images to display player and computers chosen option

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,22 @@ let warriorScore = 0;
 let computerScore = 0;
 
 const startBtn = document.getElementById("start");
+
 startBtn.addEventListener('click', function() {
+    startGame();
     console.log('Start button is clicked');
-})
+});
+
+//Function that adds the hide class to the intro screen and removes the hide class
+// from the match screen
 
 function startGame() {
+    const introScreen = document.querySelector(".intro-screen");
+    const matchScreen = document.querySelector(".playfield");
 
-}
+    console.log('game has begun');
+
+    introScreen.classList.add('hide');
+    matchScreen.classList.remove('hide');
+};
 
